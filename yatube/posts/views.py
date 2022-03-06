3ee4e288252b3858538e19d4c076cbf5ec0fb2a4
Сергей Чувычкин <chuvichkin@yandex.ru>
2022-03-06 20:47:06 +0300

    6 спринт_

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -128,16 +128,9 @@ def follow_index(request):
 @login_required
 def profile_follow(request, username):
     author = get_object_or_404(User, username=username)
-    if author == request.user:
-        return redirect(
-            'posts:profile',
-            username=username
-        )
-    follower = Follow.objects.filter(
-        user=request.user, author=author).exists()
-    if follower:
-        return redirect('posts:profile', username=username)
-    Follow.objects.create(user=request.user, author=author)
+    user = request.user
+    if user != author:
+        Follow.objects.get_or_create(user=user, author=author)
     return redirect('posts:profile', username=username)
 
 
